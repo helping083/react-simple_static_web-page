14e6c30ea620f3ccd8fbcf63ae05b98e8024689c
helping083 <helping083@gmail.com>
2019-05-17 16:19:54 +0300

    finish carousel

--- my-app/src/components/featured/carouse.js
+++ my-app/src/components/featured/carouse.js
@@ -5,54 +5,41 @@ import slide_two from '../../resources/images/slide_two.jpg';
 import slide_three from '../../resources/images/slide_three.jpg';
 
 const carouse = () => {
-    const settings = {
-        dots: false,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        pauseOnHover: false,
-        speed: 800
-    }
-    //todo - change on dynamic instead of static
+  const settings = {
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    pauseOnHover: false,
+    speed: 800
+  }
+  const slides = [slide_one, slide_two, slide_three].map((item, index) => {
     return (
+      <div key={item}>
         <div
-            className="carousel_wrapper"
-            style={{
-                height: `${window.innerHeight}px`,
-                overflow:'hidden'
-            }}
-        >
-          <Slider {...settings}>
-            <div>
-              <div 
-                className="carrousel_image"
-                style={{
-                    height: `${window.innerHeight}px`,
-                    background: `url(${slide_one})`
-                }}
-              ></div>    
-            </div>
-            <div>
-              <div 
-                className="carrousel_image"
-                style={{
-                    height: `${window.innerHeight}px`,
-                    background: `url(${slide_two})`
-                }}
-              ></div> 
-            </div>
-            <div>
-              <div 
-                className="carrousel_image"
-                style={{
-                    height: `${window.innerHeight}px`,
-                    background: `url(${slide_three})`
-                }}
-              ></div>
-            </div>
-          </Slider>    
-        </div>
+          key={item+index}
+          className="carrousel_image"
+          style={{
+            height: `${window.innerHeight}px`,
+            background: `url(${index===0 ? slide_one: index===1 ? slide_two:  slide_three})`
+          }}
+        ></div>
+      </div>
     );
+  });
+  return (
+    <div
+      className="carousel_wrapper"
+      style={{
+        height: `${window.innerHeight}px`,
+        overflow: 'hidden'
+      }}
+    >
+      <Slider {...settings}>
+        {slides}
+      </Slider>
+    </div>
+  );
 };
 
 export default carouse;
--- my-app/src/components/featured/timeUntill.js
+++ my-app/src/components/featured/timeUntill.js
@@ -3,47 +3,61 @@ import Slide from 'react-reveal/Slide';
 class timeUntill extends Component {
 
     state = {
-        days: {
-            time: null,
-            tag: 'days'
-        },
-        hours: {
-            time: null,
-            tag: 'hours'
-        },
-        minutes: {
-            time: null,
-            tag: 'minutes'
-        },
-        seconds: {
-            time: null,
-            tag: 'seconds'
-        }
+        days: null,
+        hours: null,
+        minutes: null,
+        seconds: null,
+        deadline: 'Dec, 25, 2019'
     }
 
+
+    componentDidMount() {
+        setInterval(()=>{
+            this.getTimeUntill(this.state.deadline)
+        },1000);
+    }
+    getTimeUntill(deadline) {
+      const time = Date.parse(deadline) - Date.parse(new Date());
+      if(time < 0) {
+
+      } else {
+        const seconds = Math.floor((time/1000)%60)+1;
+        const minutes = Math.floor((time/1000/60)%60);
+        const hours   = Math.floor((time/(1000*60*60))%24);
+        const days    = Math.floor(time/(1000*60*60*24));
+        this.setState({
+            days:days,
+            hours: hours,
+            seconds: seconds,
+            minutes: minutes
+        });
+        console.log(this.state)
+      }
+      
+    };
     render() {
         //todo - change on dynamic instead of static
         return (
-            <Slide left>
+            <Slide left delay={1000}>
                 <div className="countdown_wrapper">
                     <div className="countdown_top">
                         event starts in
                 </div>
                     <div className="countdown_bottom">
                         <div className="countdownItem">
-                            <div className="countdown_time">23</div>
+                            <div className="countdown_time">{this.state.days}</div>
                             <div className="countdown_tag">days</div>
                         </div>
                         <div className="countdownItem">
-                            <div className="countdown_time">2</div>
+                            <div className="countdown_time">{this.state.hours}</div>
                             <div className="countdown_tag">hours</div>
                         </div>
                         <div className="countdownItem">
-                            <div className="countdown_time">30</div>
+                            <div className="countdown_time">{this.state.minutes}</div>
                             <div className="countdown_tag">minutes</div>
                         </div>
                         <div className="countdownItem">
-                            <div className="countdown_time">50</div>
+                            <div className="countdown_time">{this.state.seconds}</div>
                             <div className="countdown_tag">seconds</div>
                         </div>
                     </div>
