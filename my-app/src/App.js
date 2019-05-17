6cc6201592e7c01e76d5bad825042e501042d57d
helping083 <helping083@gmail.com>
2019-05-18 00:04:29 +0300

    zoom and added to universal selector as library requires if i use flex

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import './resources/styles.css';
 import Header from './components/header_footer/header';
 import Featured from './components/featured/index';
+import VenueInfo from './components/venueInfo/index';
+
 
 function App() {
   return (
-    <div className="App" style={{ height:"1500px", background: 'cornflowerblue'}}>
+    <div className="App"style={{ height:"1500px", background: 'cornflowerblue'}}>
         <Header/>
         <Featured/>
+        <VenueInfo/>
     </div>
   );
 }
